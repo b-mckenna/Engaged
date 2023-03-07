3f6fff9957fb87eadace158dc7c8d4f637da9b4c
b-mckenna <brendanmckenna4@gmail.com>
2023-03-06 21:54:59 -0600

    Implement functioning

--- app/background.js
+++ app/background.js
@@ -4,27 +4,44 @@ chrome.runtime.onInstalled.addListener(function () {
     console.log("Extension is loaded.");
 });
 
-   
-// Set the timer element to the selected duration
-var timerEl = document.getElementById('timer');
-timerEl.innerText = duration;
-
-    
-// Start the countdown
-var countdownInterval = setInterval(function() {
-    // Decrement the duration
-    durationInMillis -= 1000;
-    var remainingMinutes = Math.floor(durationInMillis / 60000);
-    var remainingSeconds = Math.floor((durationInMillis % 60000) / 1000);
-
-    // Update the timer element
-    requestAnimationFrame(function() {
-      timerEl.innerText = remainingMinutes + ":" + (remainingSeconds < 10 ? "0" : "") + remainingSeconds;
-    });
-
-    // Check if the countdown is finished
-    if (durationInMillis <= 0) {
-        clearInterval(countdownInterval);
-        alert("Time's up!");
+// background.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.task && request.duration && request.startTime) {
+        // Do something with the received data
+        console.log(request.task);
+        console.log(request.duration);
+        console.log(request.startTime);
     }
-}, 1000);
+    if (request.action === "callMyFunction") {
+      startTimer(duration);
+      sendResponse({ message: "Function called successfully." });
+    }
+
+    return new Promise((resolve, reject) => {
+        // Resolve the promise with the result
+        resolve("success");
+        reject("whoops!")
+    });
+});
+
+function startTimer(duration) {
+    let timerDisplay = document.getElementById("timer");
+    timerDisplay.style.display = "block";
+    let endTime = Date.now() + duration * 1000;
+    countdown = setInterval(() => {
+      let remainingTime = Math.round((endTime - Date.now()) / 1000);
+      if (remainingTime < 0) {
+        clearInterval(countdown);
+        chrome.windows.create({
+          url: "notification.html",
+          type: "popup",
+          width: 400,
+          height: 200
+        });
+      } else {
+        let minutes = Math.floor(remainingTime / 60);
+        let seconds = remainingTime % 60;
+        timerDisplay.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+      }
+    }, 1000);
+}
--- app/popup.js
+++ app/popup.js
@@ -1,30 +1,45 @@
-let countdown;
-const startButton = document.getElementById("start");
-startButton.addEventListener("click", () => {
-  const task = document.getElementById("task").value;
-  const duration = document.querySelector('input[name="duration"]:checked').value;
-  chrome.storage.sync.set({ task, duration, start: Date.now() });
-  startTimer(duration);
-});
+document.addEventListener("DOMContentLoaded", function() {
+/*   chrome.storage.sync.clear(() => {
+    console.log("Storage cleared.");
+  }); */
+  
+  let countdown;
+  const startButton = document.getElementById("start");
+  startButton.addEventListener("click", () => {
+    const task = document.getElementById("task").value;
+    const duration = document.querySelector('input[name="duration"]:checked').value;
 
-function startTimer(duration) {
-  let timerDisplay = document.getElementById("timer");
-  timerDisplay.style.display = "block";
-  let endTime = Date.now() + duration * 1000;
-  countdown = setInterval(() => {
-    let remainingTime = Math.round((endTime - Date.now()) / 1000);
-    if (remainingTime < 0) {
-      clearInterval(countdown);
-      chrome.windows.create({
-        url: "notification.html",
-        type: "popup",
-        width: 400,
-        height: 200
-      });
-    } else {
-      let minutes = Math.floor(remainingTime / 60);
-      let seconds = remainingTime % 60;
-      timerDisplay.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-    }
-  }, 1000);
-}
+    chrome.storage.sync.set({ task, duration, start: Date.now() });
+    startTimer(duration);
+  });
+
+  // local timer
+/*   function startTimer(duration) {
+    let timerDisplay = document.getElementById("timer");
+    timerDisplay.style.display = "block";
+    let endTime = Date.now() + duration * 1000;
+    countdown = setInterval(() => {
+      let remainingTime = Math.round((endTime - Date.now()) / 1000);
+      if (remainingTime < 0) {
+        clearInterval(countdown);
+        chrome.windows.create({
+          url: "notification.html",
+          type: "popup",
+          width: 400,
+          height: 200
+        });
+      } else {
+        let minutes = Math.floor(remainingTime / 60);
+        let seconds = remainingTime % 60;
+        timerDisplay.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+      }
+    }, 1000);
+  } */
+
+  //TODO: move timer to background
+  function startTimer(duration) {
+    chrome.runtime.sendMessage({ action: "callMyFunction", task: task, duration: duration, startTime: Date.now }, function(response) {
+      console.log(response);
+    });
+  }
+});
